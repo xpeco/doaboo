c8534fd2d0612dce768428945520fcb7eafbb520
cdoblado <cdoblado@lane.(none)>
2010-04-16 14:20:17 +0200

    one use library eliminated, not necessary

--- draft/cgi/t.pl
+++ draft/cgi/t.pl
@@ -2,12 +2,11 @@
 
 use strict;
 use warnings;
-use XML::Simple;
 use DBCONN;
 
 my $dbh=DBCONN->new; # connect
-my $r=$dbh->DBCONN::rawget('select DATE from ALARMS limit 1','ARRAY'); # query
-#my $r=$dbh->prepare('show fields from ALARMS')->execute; # plain
+my $r=$dbh->DBCONN::rawget('SELECT * FROM machines LIMIT 1','ARRAY'); # query
+#my $r=$dbh->prepare('show fields from machines')->execute; # plain
 
 print "$r\n"; # show
 
