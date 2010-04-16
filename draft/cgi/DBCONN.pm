a8e9140528382b7b17fdd2971796ce3df21a0f8d
Juan José 'Peco' San Martín <jsanmartin@gmail.com>
2010-04-16 11:34:07 +0200

    Add basic support to read conf file and run queries

--- draft/cgi/DBCONN.pm
+++ draft/cgi/DBCONN.pm
@@ -5,6 +5,7 @@ package DBCONN;
 use strict;
 use warnings;
 use Apache::DBI;
+use XML::Simple;
 
 $Apache::DBI::DEBUG = 2; #it works under mod_perl only
 
@@ -16,15 +17,45 @@ sub new{
    return $self;
 }
 
-sub _init {
- my $dbh = DBI->connect
- ("DBI:mysql:doaboo:localhost", 'doaboo', 'doaboo',
-   {
-    PrintError => 1, # warn() on errors
-    RaiseError => 0, # don't die on error
-    AutoCommit => 1, # commit executes immediately
+sub rawget
+# Makes queries to the database (db) returning the rows in the
+# same way the EXIN functions do.
+{
+	my $dbh=shift;
+	unless (ref $self){
+      print "Error, should call surname() with an object, not a class";
    }
- ) or die "Cannot connect to database: $DBI::errstr";
- return $dbh;
+	my $query=shift;
+	my $format=shift;
+ 	my $do=$dbh->prepare($query);
+ 	$do->execute;
+	if (not defined $format){return $do->fetchall_arrayref({});}
+	elsif ($format eq 'ARRAY'){
+		my $list='(';
+		while(my @e=$do->fetchrow_array){$list.=$e[0].',';}
+		$list=~s/\,\Z/\)/;
+		if($list eq '('){$list='()';} # if no results come from the Q
+		return $list;
+ 	}
+	elsif ($format eq 'YARRA'){
+   	my $list=')';
+		while(my @e=$do->fetchrow_array){$list=','.$e[0].$list;}
+		$list=~s/\A\,/\(/;
+		if($list eq ')'){$list='()';} # if no results come from the Q
+		return $list;
+	}
+	else {return $do->fetchall_arrayref({});}
+}
+
+sub _init {
+	my $conf=XML::Simple::XMLin("./doaboo.conf");
+	my $dbh = DBI->connect("DBI:mysql:$conf->{DDBB}->{NAME}:$conf->{DDBB}->{HOST}", $conf->{DDBB}->{USER},$conf->{DDBB}->{PASS},
+		{
+			PrintError => 1, # warn() on errors
+			RaiseError => 0, # don't die on error
+			AutoCommit => 1, # commit executes immediately
+   	}
+ 		) or die "Cannot connect to database: $DBI::errstr";
+	return $dbh;
 }
 1;
