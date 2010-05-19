e1472c022431dcd6e4a1007da9bae69870508693
Teresa Carapeto <tcarapeto@localhost.localdomain>
2010-05-19 11:30:26 +0200

    padel2doaboo v2: TOPICS, ATTRIBUTES, ACTIONS, FIELDS, SCRIPTS, MENUS

--- draft/padel2doaboo/DB_UPDATE.pm
+++ draft/padel2doaboo/DB_UPDATE.pm
@@ -19,29 +19,147 @@ sub get_id {
 				my $dbh=shift;
         		unless (ref $dbh){ print "Error, should call rawget() with an object, not a class\n";};
 				my $item=shift;
-            my $query=$dbh->prepare("select id from doaboo_topics where name=\'$item->{NAME}\'");
+				my $format=shift;
+            my $query=$dbh->prepare("select id from doaboo_"."$format"." where name=\'$item->{NAME}\'");
       		$query->execute;
       		my @id_item=$query->fetchrow_array;
 				return $id_item[0];
 }
 
-sub create {
-				my $dbh=shift;
-				unless (ref $dbh){ print "Error, should call rawget() with an object, not a class\n";};
-				my $item=shift;
-				$item->{DESC} = $item->{NAME} if not defined $item->{DESC};
-				$item->{HINT} = '' if not defined $item->{HINT};
-				my $query=$dbh->prepare("insert into doaboo_topics set name=\'$item->{NAME}\',description=\'$item->{DESC}\',hint=\'$item->{HINT}\'")->execute;
-				}
-
-sub update {
+sub record {
 				my $dbh=shift;
 				unless (ref $dbh){ print "Error, should call rawget() with an object, not a class\n";};
 	         my $item=shift;
 				my $id=shift;
-   	      $item->{DESC} = $item->{NAME} if not defined $item->{DESC};
-      	   $item->{HINT} = '' if not defined $item->{HINT};
- 				my $query=$dbh->prepare("update doaboo_topics set name=\'$item->{NAME}\',description=\'$item->{DESC}\',hint=\'$item->{HINT}\' where id=\'$id'")->execute;
+				my $format=shift;
+				my $topic_id=shift;
+				my $action_id=shift;
+   	      my $string='';
+				$item->{DESC} = $item->{NAME} if not defined $item->{DESC};
+				if (defined $item->{DESC}) {
+					my $code='';
+					$code=$item->{DESC};
+					$code=~s/\'/\\\'/g;
+               $string=" description=\"$code\"";
+            }
+				my @list=('NAME','HINT','TYPE','REQUIRED','RECALCULATE','KEY_CAPTION','LIST','SUBTYPE','PRIVATE');
+         	foreach my $elem (@list){
+            my $table=lc($elem);
+            if (defined $item->{$elem}) {
+               $string="$string".", $table=\'$item->{$elem}\'";
+            }
+         	}
+				if (defined $item->{KEY}) {
+               $string="$string".", clave=\'$item->{KEY}\'";
+            }
+				@list=('RANGE','HIDE','CAPTION');
+            foreach my $elem(@list){
+               $table=lc($elem);
+               if (defined $item->{$elem}->{BLOCK}[0]) {
+                  my $code='';
+                  $code= $item->{$elem}->{BLOCK}[0];
+                  $code=~s/\'/\\\'/g;
+                  $code=~s/\"/\\\"/g;
+                  $code=~s/\./\\\./g;
+                  $code=~s/\*/\\\*/g;
+                  $string="$string"." ,$table='Y', $table"."_logic=\"$code\"";
+               }
+            }
+				if (defined $item->{VALUE}->{BLOCK}[0]) {
+               $code='';
+               $code= $item->{VALUE}->{BLOCK}[0];
+					$code=~s/\\/\\\\/g;
+               $code=~s/\'/\\\'/g;
+               $code=~s/\"/\\\"/g;
+               $code=~s/\./\\\./g;
+               $code=~s/\*/\\\*/g;
+               $string="$string"." ,calculated_logic=\"$code\"";
+            }
+				if ((defined $topic_id) and (($format eq 'attributes') or ($format eq 'actions'))) {
+					$string="$string"." ,topic=\'$topic_id\'";
+				}
+				if ((defined $action_id) and ($format eq 'fields')) {
+					my $att=$dbh->prepare("select id from doaboo_attributes where name=\'$item->{NAME}\' and topic=\'$topic_id\'");
+					$att->execute;
+					my @att=$att->fetchrow_array;
+					if (defined $att[0]) {
+						$string="$string"." ,attribute=\'$att[0]\', action=\'$action_id\'";
+					}
+				}
+				
+				if ((defined $item->{TYPE}) and ($format eq 'menus') and ($item->{TYPE} eq 'TOPIC')) {
+					my $topic=$dbh->prepare("select id from doaboo_topics where name=\'$item->{NAME}\'");
+               $topic->execute;
+               my @topic=$topic->fetchrow_array;
+					if (defined $topic[0]) {
+                  $string="$string".", topic=\'$topic[0]\'";
+               }
+				}
+				if (defined $item->{RELATION}) {
+            	my $relation=$dbh->prepare("select id from doaboo_topics where name=\'$item->{RELATION}\'");
+            	$relation->execute;
+            	my @relation=$relation->fetchrow_array;
+					if (defined $relation[0]) {
+            		$string="$string".", relation=\'$relation[0]\'";
+					}
+            }
+				@list=('PRESCRIPT','POSTSCRIPT','ENDSCRIPT');
+				my $i=0;
+            foreach my $elem(@list){
+               $i++;
+               if (defined $item->{$elem}->{BLOCK}[0]) {
+                  my $code='';
+                  $code= $item->{$elem}->{BLOCK}[0];
+						$code=~s/\\/\\\\/g;
+                  $code=~s/\'/\\\'/g;
+                  $code=~s/\"/\\\"/g;
+                  $code=~s/\./\\\./g;
+                  $code=~s/\*/\\\*/g;
+                  $string="$string"." , step"."$i"."=\"$code\"";
+               }
+            }
+				if (($format eq 'attributes') and (defined $item->{BLOCK}[0])) {
+						my $code='';
+                  $code= $item->{$elem}->{BLOCK}[0];
+                  $code=~s/\\/\\\\/g;
+                  $code=~s/\'/\\\'/g;
+                  $code=~s/\"/\\\"/g;
+                  $code=~s/\./\\\./g;
+                  $code=~s/\*/\\\*/g;
+                  $string="$string"." , logic=\"$code\"";
+				}
+				if (defined $item->{SCRIPT_ACTION}->{BLOCK}[0]) {
+						my $code='';
+                  $code= $item->{SCRIPT_ACTION}->{BLOCK}[0];
+                  $code=~s/\\/\\\\/g;
+                  $code=~s/\'/\\\'/g;
+                  $code=~s/\"/\\\"/g;
+                  $code=~s/\./\\\./g;
+                  $code=~s/\*/\\\*/g;
+                  $string="$string"." , logic=\"$code\"";
+				}
+				if (defined $item->{PARENT}) {
+					$string="$string"." ,parent=\'$item->{PARENT}\'";
+				}
+				if (defined $item->{URL}) {
+						my $code='';
+                  $code= $item->{URL};
+                  $code=~s/\\/\\\\/g;
+                  $code=~s/\'/\\\'/g;
+                  $code=~s/\"/\\\"/g;
+                  $code=~s/\./\\\./g;
+                  $code=~s/\//\\\//g;
+						$code=~s/\:/\\\:/g;
+						$code=~s/\-/\\\-/g;
+						$code=~s/\_/\\\_/g;
+						$string="$string"." ,url=\"$code\"";
+				}
+				if (defined $id) {
+ 					my $query=$dbh->prepare("update doaboo_"."$format"." set "."$string"." where id=\'$id\'")->execute;
+				}
+				else {
+            	my $query=$dbh->prepare("insert into doaboo_"."$format"." set "."$string")->execute;
+				}
 				}
 
 
--- draft/padel2doaboo/xml_parser.pl
+++ draft/padel2doaboo/xml_parser.pl
@@ -15,7 +15,7 @@ my $list=$xfile->{SOURCE};
 foreach my $source(@$list){;push(@source_XMLs,$source);}
 my $i=0;
 foreach my $file (@source_XMLs) {
-   my $xs1= new XML::Simple (keeproot=>1, forcearray=>['OBJECT','ATTRIBUTE','METHOD','FIELD','BLOCK']);
+   my $xs1= new XML::Simple (keeproot=>1, forcearray=>['OBJECT','ATTRIBUTE','METHOD','FIELD','BLOCK','MENU','SCRIPT']);
    my $result=eval{$XMLin[$i]=$xs1->XMLin($file) || die "can't XMLin $file: $!";};
    if($result==0){print "$@";if(<STDIN>){};exit();}
    $i++;
@@ -24,49 +24,39 @@ my $dbh = DB_UPDATE ->new();
 for ($i=0;$i<@XMLin;$i++) {
    my $objects= $XMLin[$i]->{MODULE}->{OBJECT};
    foreach my $object (@$objects) {
-		my $object_id=$dbh->DB_UPDATE::get_id($object);
-		if (defined $object_id) {
-			$dbh->DB_UPDATE::update($object,$object_id);
-		}
-		else {
-			$dbh->DB_UPDATE::create($object);
-			$object_id=$dbh->DB_UPDATE::get_id($object);
-		}
+		my $object_id=$dbh->DB_UPDATE::get_id($object,'topics');
+	   $dbh->DB_UPDATE::record($object,$object_id,'topics');
+		$object_id=$dbh->DB_UPDATE::get_id($object,'topics');
 		my $attributes = $object->{ATTRIBUTE};
       foreach my $attrib (@$attributes) {
-			### GRABAR ATTRIBUTE ###
-			########################
-			### get id_attribute
-			### if defined id_attribute
-			###	update_attribute
-			### else 
-			### 	insert_attribute
-			###	get_attribute
-			#######################
-		}
+			my $attribute_id=$dbh->DB_UPDATE::get_id($attrib,'attributes');
+		   $dbh->DB_UPDATE::record($attrib,$attribute_id,'attributes',$object_id);
+			$attribute_id=$dbh->DB_UPDATE::get_id($attrib,'attributes');
+      }
 		my $methods = $object->{METHOD};
-      foreach my $method (@$methods) {
-			### GRABAR ACTION ###
-			#####################
-			### get id_action
-			### if defined id_action
-			### 	update_action
-			### else
-			### 	insert_action
-			### 	get id_action
-			#####################
+		foreach my $method (@$methods) {
+			my $method_id=$dbh->DB_UPDATE::get_id($method,'actions');
+			$dbh->DB_UPDATE::record($method,$method_id,'actions',$object_id);
+			$method_id=$dbh->DB_UPDATE::get_id($method,'actions');
 			my $fields = $method->{FIELD};
          foreach my $field (@$fields) {
-				### GRABAR FIELD ###
-				####################
-				### get id_field
-				### get id_attribute
-				### if defined id_field 
-				### 	update_field
-				### else
-				### 	insert_field
-				####################
+				my $field_id=$dbh->DB_UPDATE::get_id($field,'fields');
+				$dbh->DB_UPDATE::record($field,$field_id,'fields',$object_id,$method_id);
 			}
 		}
 	}	
- }
+}
+for ($i=0;$i<@XMLin;$i++) {
+	my $menus=$XMLin[$i]->{MODULE}->{MENU};
+	foreach my $menu(@$menus) {
+		my $menu_id=$dbh->DB_UPDATE::get_id($menu,'menus');
+		$dbh->DB_UPDATE::record($menu,$menu_id,'menus');
+	}
+}
+for ($i=0;$i<@XMLin;$i++) {
+   my $scripts=$XMLin[$i]->{MODULE}->{SCRIPT};
+   foreach my $script(@$scripts) {
+      my $script_id=$dbh->DB_UPDATE::get_id($script,'scripts');
+      $dbh->DB_UPDATE::record($script,$script_id,'scripts');
+   }
+}
