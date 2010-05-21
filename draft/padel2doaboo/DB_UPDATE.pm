a4e1885d074ef7843324dde1d2921b31d270b996
Teresa Carapeto <tcarapeto@localhost.localdomain>
2010-05-21 10:27:24 +0200

    Code improvments

--- draft/padel2doaboo/DB_UPDATE.pm
+++ draft/padel2doaboo/DB_UPDATE.pm
@@ -11,7 +11,7 @@ sub new{
         my $class=shift;
         my $self={@_};
         bless($self, $class);
-        my $dbh=$self->_init; # make the connection
+        my $dbh=$self->_init;
         return $dbh;
 }
 
@@ -42,15 +42,15 @@ sub record {
 		$code=~s/\'/\\\'/g;
 		$string=" description=\"$code\"";
 	}
-	my @list=('NAME','HINT','TYPE','REQUIRED','RECALCULATE','KEY_CAPTION','LIST','SUBTYPE','PRIVATE');
+	my @list=('NAME','HINT','TYPE','REQUIRED','RECALCULATE','KEY_CAPTION','LIST','SUBTYPE','PRIVATE','PARENT','URL');
 	foreach my $elem (@list){
 		my $table=lc($elem);
 		if (defined $item->{$elem}) {
-			$string="$string".", $table=\'$item->{$elem}\'";
+			$string.=", $table=\'$item->{$elem}\'";
 		}
 	}
 	if (defined $item->{KEY}) {
-		$string="$string".", clave=\'$item->{KEY}\'";
+		$string.=", clave=\'$item->{KEY}\'";
 	}
 	@list=('RANGE','HIDE','CAPTION');
 	foreach my $elem(@list){
@@ -83,7 +83,7 @@ sub record {
 			       $code=~s/\s+$//;
 		       }
 		       $code = join("\} ", @code);
-		       $string="$string"." ,$table='Y', $table"."_logic=\"$code\"";
+		       $string.=" ,$table='Y', $table"."_logic=\"$code\"";
                }
        }
        if (defined $item->{VALUE}->{BLOCK}[0]) {
@@ -115,17 +115,17 @@ sub record {
 		 $code=~s/\s+$//;
 	       }
 	       $code = join("\} ", @code);
-               $string="$string"." ,calculated_logic=\"$code\"";
+               $string.=" ,calculated_logic=\"$code\"";
        }
        if ((defined $topic_id) and (($format eq 'attributes') or ($format eq 'actions'))) {
-	       $string="$string"." ,topic=\'$topic_id\'";
+	       $string.=" ,topic=\'$topic_id\'";
        }
        if ((defined $action_id) and ($format eq 'fields')) {
 	       my $att=$dbh->prepare("select id from doaboo_attributes where name=\'$item->{NAME}\' and topic=\'$topic_id\'");
 	       $att->execute;
 	       my @att=$att->fetchrow_array;
 	       if (defined $att[0]) {
-		       $string="$string"." ,attribute=\'$att[0]\', action=\'$action_id\'";
+		       $string.=" ,attribute=\'$att[0]\', action=\'$action_id\'";
 	       }
        }
        if ((defined $item->{TYPE}) and ($format eq 'menus') and ($item->{TYPE} eq 'TOPIC')) {
@@ -133,7 +133,7 @@ sub record {
                $topic->execute;
                my @topic=$topic->fetchrow_array;
 	       if (defined $topic[0]) {
-		       $string="$string".", topic=\'$topic[0]\'";
+		       $string.=", topic=\'$topic[0]\'";
                }
        }
        if (defined $item->{RELATION}) {
@@ -141,7 +141,7 @@ sub record {
 	       $relation->execute;
 	       my @relation=$relation->fetchrow_array;
 	       if (defined $relation[0]) {
-		       $string="$string".", relation=\'$relation[0]\'";
+		       $string.=", relation=\'$relation[0]\'";
 	       }
        }
        @list=('PRESCRIPT','POSTSCRIPT','ENDSCRIPT');
@@ -177,7 +177,7 @@ sub record {
 		           $code=~s/\s+$//;
 	               }
 	               $code = join("\} ", @code);
-		       $string="$string"." , step"."$i"."=\"$code\"";
+		       $string.=" , step"."$i"."=\"$code\"";
 	       }
        }
        if (defined $item->{BLOCK}[0]) {
@@ -209,23 +209,7 @@ sub record {
 		 $code=~s/\s+$//;
 	       }
 	       $code = join("\} ", @code);
-	       $string="$string"." , logic=\"$code\"";
-       }
-       if (defined $item->{PARENT}) {
-	       $string="$string"." ,parent=\'$item->{PARENT}\'";
-       }
-       if (defined $item->{URL}) {
-	       my $code='';
-	       $code= $item->{URL};
-	       $code=~s/\\/\\\\/g;
-	       $code=~s/\'/\\\'/g;
-	       $code=~s/\"/\\\"/g;
-	       $code=~s/\./\\\./g;
-	       $code=~s/\//\\\//g;
-	       $code=~s/\:/\\\:/g;
-	       $code=~s/\-/\\\-/g;
-	       $code=~s/\_/\\\_/g;
-	       $string="$string"." ,url=\"$code\"";
+	       $string.=" , logic=\"$code\"";
        }
        if (defined $id) {
 	       my $query=$dbh->prepare("update doaboo_"."$format"." set "."$string"." where id=\'$id\'")->execute;
